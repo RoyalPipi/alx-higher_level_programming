66322ae4b7a50b1e1f8f0593a84055ddaa7c1210
Obiekwuye Princess Amaka <royalpipi77@gmail.com>
2023-01-04 13:40:22 -0800

    fix

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# 101-nqueens.py
+# Obiekwuye Princess Amaka:x
 """Solves the N-queens puzzle.
 Determines all possible solutions to placing N
 N non-attacking queens on an NxN chessboard.
